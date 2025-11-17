ee3b8dc4b83ac9d02bd8c615d7a467c101ecc784
Conner Penn <Conner Penn>
2025-11-16 21:11:41 -0700

    update for render deployment

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template, abort
+import os
 
 app = Flask(__name__)
 
@@ -55,4 +56,5 @@ def about():
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
